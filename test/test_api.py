71a6af322c9bd0313d2e9f01a36586a28ae8f13b
Guilherme Polo <gp@coinapult.com>
2015-02-19 03:10:59 -0500

    started tests for handling watchers through the api

--- test/test_api.py
+++ test/test_api.py
@@ -95,6 +95,123 @@ class TestQuery(unittest.TestCase):
         self.assertEqual(res.get('data', ''), None)
 
 
+class TestWatch(unittest.TestCase):
+
+    def setUp(self):
+        self.evt_list = set()
+
+    def tearDown(self):
+        for evt in self.evt_list:
+            print 'removing %s' % evt
+            api.cancel_watch(evt)
+
+    def test_watchaddress(self):
+        cb = 'http://localhost:10000/abc'
+
+        for invalid in ('a', '?', '', None):
+            res = api.watch_address(invalid, cb)
+            self.assertEqual(res.get('error'), 'invalid address')
+
+        for test in ('1' * 30, ):  # Addresses are not validated here.
+            res = api.watch_address(test, cb)
+            self._check_watchaddress(res, cb, test)
+
+    def test_watchnewblocks(self, etype='newblock', func=api.watch_newblocks):
+        for invalid in (None, ''):
+            res = func(invalid)
+            if 'id' in res:
+                # This shouldn't happen.
+                self.evt_list.add(res['id'])
+                self.fail('succeeded with callback = "%s"' % invalid)
+
+            self.assertIn('error', res)
+            self.assertTrue(res['error'].startswith('invalid callback '))
+
+        cb = 'http://localhost:10000/abc'
+        res = func(cb)
+        self._check_watchblock(res, cb, etype)
+
+    def test_watchdiscblocks(self):
+        self.test_watchnewblocks('discblock', func=api.watch_discblock)
+
+    def test_watchnewblocks_roundtrip(self):
+        # Active -> Active -> Cancel -> Activate again using the same callback.
+        cb = 'http://localhost:20004/abcdef'
+
+        # First Active.
+        res = api.watch_newblocks(cb)
+        self._check_watchblock(res, cb, 'newblock')
+
+        # Try activating again.
+        res2 = api.watch_newblocks(cb)
+        self.assertEqual(res2.get('msg'), 'already exists')
+        self.assertEqual(res2.get('code'), 409)
+        self.assertEqual(res2.get('success', ''), False)
+
+        # Cancel it.
+        res = api.cancel_watch(res['id'])
+        self.assertEqual(res, {'success': True})
+
+        # Activate again.
+        res = api.watch_newblocks(cb)
+        self._check_watchblock(res, cb, 'newblock')
+
+    def test_twosubs_oneaddress(self):
+        cb1 = 'http://localhost:12345/abc'
+        cb2 = 'http://localhost:12345/def'
+        addy = '12' * 15
+
+        evt1 = api.watch_address(addy, cb1)
+        self._check_watchaddress(evt1, cb1, addy)
+        evt2 = api.watch_address(addy, cb2)
+        self._check_watchaddress(evt2, cb2, addy)
+
+        self.assertEqual(evt1['address'], evt2['address'])
+        self.assertNotEqual(evt1['id'], evt2['id'])
+
+    def test_onesub_twoaddresses(self):
+        cb1 = 'http://localhost:12345/xyz'
+        addy1 = '13' * 15
+        addy2 = '14' * 15
+
+        evt1 = api.watch_address(addy1, cb1)
+        self._check_watchaddress(evt1, cb1, addy1)
+        # Currently there is a limitation in how subscribers are stored
+        # which causes all watch addresses for a given subscriber to
+        # be active or inactive.
+        #
+        # So if you subscribe to two different addresses, then cancel
+        # the subscription, and then subscribe to the same two addresses
+        # again the second call will report that the subscription is already
+        # active.
+        evt2 = api.watch_address(addy2, cb1)
+        if 'code' in evt2 and evt2['code'] == 409:
+            # See comment above.
+            evt2 = evt1.copy()
+            evt2['address'] = addy2
+        else:
+            self.fail('unexpected behavior: %r' % evt2)
+
+        self.assertEqual(evt1['id'], evt2['id'])
+        self.assertNotEqual(evt1['address'], evt2['address'])
+
+    def _check_watchaddress(self, res, cb, address):
+        self.assertIn('id', res)
+        self.evt_list.add(res['id'])
+        self.assertEqual(res.get('success'), True)
+        self.assertEqual(res.get('callback'), cb)
+        self.assertEqual(res.get('address'), address)
+        self.assertEqual(res.get('type'), 'address')
+
+    def _check_watchblock(self, res, cb, etype):
+        self.assertIn('id', res)
+        self.evt_list.add(res['id'])
+
+        self.assertEqual(res.get('success'), True)
+        self.assertEqual(res.get('callback'), cb)
+        self.assertEqual(res.get('type'), etype)
+
+
 def _get_txid(blockheight):
     block = api.query(blockheight)
     for tx in block['data']['rawtx']:
@@ -112,4 +229,4 @@ def _get_address(blockheight):
 
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(verbosity=2)
